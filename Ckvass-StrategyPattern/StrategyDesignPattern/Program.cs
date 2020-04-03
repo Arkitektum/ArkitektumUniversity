b1720ac39f34dd8e61f2b58bca7bebc7d306b8ea
Tor Oskar Ova Johnsen <toroskar@arkitektum.no>
2020-04-03 20:03:28 +0200

    Strategy implemented

--- Ckvass-StrategyPattern/StrategyDesignPattern/Program.cs
+++ Ckvass-StrategyPattern/StrategyDesignPattern/Program.cs
@@ -6,44 +6,82 @@ namespace StrategyDesignPattern
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Strategy!");
+
+
+            // Randomly get one of the enum values
+            var incomingAltinnData = (SupportedProcessingPathForAltinnForms) new Random().Next(Enum.GetNames(typeof(SupportedProcessingPathForAltinnForms)).Length);
+            Console.WriteLine($"We are processing an Altinn request of type {incomingAltinnData}");
+
+            // Select processing strategy
+            IProcessAltinnDownloadQueueItemStrategy processStrategy;
+
+            switch (incomingAltinnData)
+            {
+                case SupportedProcessingPathForAltinnForms.Ship:
+                    processStrategy = new ProcessSendToKommune();
+                    break;
+
+                case SupportedProcessingPathForAltinnForms.Distribute:
+                    processStrategy = new ProcessDistribution();
+                    break;
+                
+                case SupportedProcessingPathForAltinnForms.Notification:
+                    processStrategy = new ProcessSendToApplicant();
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+
+            // Run the strategy
+            processStrategy.ProcessData();
         }
     }
 
 
+    public enum SupportedProcessingPathForAltinnForms
+    {
+        Ship, // Send to kommune
+        Distribute, // Distribute nabovarsel, ansvarrett, Kos
+        Notification // Send data back to the requester 
+    };
 
 
 
+
+    // INTERFACE
     internal interface IProcessAltinnDownloadQueueItemStrategy
     {
-        void ProcessData(string archiveReference);
+        void ProcessData();
     }
 
+
+    // STRATEGY 1
     internal class ProcessDistribution : IProcessAltinnDownloadQueueItemStrategy
     {
-        public void ProcessData(string archiveReference)
+        public void ProcessData()
         {
-            Console.WriteLine($"{archiveReference} is being processed for distribution");
+            Console.WriteLine("The Altinn Archive Reference is being processed for distribution");
         }
     }
 
+
+    // STRATEGY 2
     internal class ProcessSendToKommune : IProcessAltinnDownloadQueueItemStrategy
     {
-        public void ProcessData(string archiveReference)
+        public void ProcessData()
         {
-            Console.WriteLine($"{archiveReference} is being sent to kommune through KS FIKS  SvarUt");
+            Console.WriteLine("The Altinn Archive Reference is being sent to kommune through KS FIKS  SvarUt");
         }
     }
 
+
+    // STRATEGY 2
     internal class ProcessSendToApplicant : IProcessAltinnDownloadQueueItemStrategy
     {
-        public void ProcessData(string archiveReference)
+        public void ProcessData()
         {
-            Console.WriteLine($"{archiveReference} is being sent to Ansvarlig Sokers inbox in Altinn");
+            Console.WriteLine("The Altinn Archive Reference is being sent to Ansvarlig Sokers inbox in Altinn");
         }
     }
-
-
-
-
 }
