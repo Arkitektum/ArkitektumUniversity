910ffed84d03cc67ffdeabfdb138b59f52da8646
Tor Oskar Ova Johnsen <toroskar@arkitektum.no>
2020-04-20 22:44:24 +0200

    Humble beginnings

---
+++ Ckvass-RetryWithPolly/Ckvass-RetryWithPolly/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ckvass_RetryWithPolly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
