aaba220bc7c07f2b02dc4a914d59471d61e5f817
Tor Oskar Ova Johnsen <toroskar@arkitektum.no>
2020-06-15 08:25:24 +0200

    Commentst

--- Ckvass-RetryWithPolly/Ckvass-RetryWithPolly/Program.cs
+++ Ckvass-RetryWithPolly/Ckvass-RetryWithPolly/Program.cs
@@ -1,7 +1,12 @@
-﻿using System;
+using System;
 
 namespace Ckvass_RetryWithPolly
 {
+
+    // https://github.com/App-vNext/Polly
+    // http://www.thepollyproject.org/
+    // https://github.com/App-vNext/Polly-Samples
+
     class Program
     {
         static void Main(string[] args)
